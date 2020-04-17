850b4bfd1ca33f01d22d4339975f73c68940e676
Vurdulak <andrey.bessel@gmail.com>
2020-04-17 17:39:00 +0300

    blah

---
+++ LearningLinQ/LearningLinQ/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace LearningLinQ
+{
+    /// <summary>
+    /// loads xml data from file
+    /// </summary>
+    class MainClass
+    {
+
+        static void Main()
+        {
+            XElement foods = XElement.Load(@"/Users/l.apunkt/Desktop/Code Learning/FileExamples/Foods.xml");
+            IEnumerable<string> myFood =
+                from food in foods.Descendants("breakfast_menu")
+                where food.Descendants("food")
+                select (string)food.Attribute("name");
+            foreach (var item in myFood)
+            {
+                Console.WriteLine(item);
+            }
+            
+
+            
+
+
+
+        }
+    }
+}
