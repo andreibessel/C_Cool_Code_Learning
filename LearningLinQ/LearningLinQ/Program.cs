d6a278f9a20c4dae4795070339ea32b34f09d899
Vurdulak <andrey.bessel@gmail.com>
2020-04-19 11:21:25 +0300

    Adding

--- LearningLinQ/LearningLinQ/Program.cs
+++ LearningLinQ/LearningLinQ/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Xml.Linq;
+using System.Data.SqlClient;
 
 namespace LearningLinQ
 {
@@ -11,15 +11,23 @@ namespace LearningLinQ
     class MainClass
     {
 
-        static void Main()
+        public static void Main()
         {
+            string connectionString = @"Server=localhost;Database=Foods;User Id=sa;Password=1Secure*Password1;";
+
+            CreateCommand(@"INSERT INTO dbo.Oranges (ID,Name) VALUES('1', 'colorabi'); ",connectionString);
             
-            
+           
+        }
 
-            
-
-
-
+    private static void CreateCommand(string queryString, string connectionString)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Connection.Open();
+                command.ExecuteNonQuery();
+            }
         }
     }
 }
