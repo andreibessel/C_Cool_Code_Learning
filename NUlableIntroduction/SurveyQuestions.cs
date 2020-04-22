3103b8c1c949e3c243504de23df88124c5bbc47e
Andrey <Andrey@Andreys-iMac.local>
2020-04-22 17:13:54 +0300

    still adding

---
+++ NUlableIntroduction/SurveyQuestions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NUlableIntroduction
+{
+    public enum QuestionType
+    {
+        YesNo,
+        Number,
+        Text
+    }
+    public class SurveyQuestion
+    {
+        public string QuestionText { get; }
+        public QuestionType QuestionType { get; }
+
+        public SurveyQuestion(QuestionType questionType, string text) =>
+            (QuestionType, QuestionText) = (questionType, text);
+    }
+    public class SurveyRun
+    {
+
+
+        private List<SurveyQuestion> surveyQuestionsList =
+            new List<SurveyQuestion>();
+
+        public void AddQuestion(QuestionType questionType, string question) =>
+            AddQuestion(new SurveyQuestion(questionType, question));
+
+        public void AddQuestion(SurveyQuestion surveyQuestion) =>
+            surveyQuestionsList.Add(surveyQuestion);
+
+        private List<SurveyResponse>? respondents;
+        public void PerformSurvey(int numberOfRespondents)
+        {
+            int repondentsConsenting = 0;
+            respondents = new List<SurveyResponse>();
+            while (repondentsConsenting < numberOfRespondents)
+            {
+                var respondent = SurveyResponse.GetRandomId();
+                if (respondent.AnswerSurvey(surveyQuestionsList))
+                    repondentsConsenting++;
+                respondents.Add(respondent);
+            }
+        }
+        public IEnumerable<SurveyResponse> AllParticipants => (respondents ?? Enumerable.Empty<SurveyResponse>());
+        public ICollection<SurveyQuestion> Questions => surveyQuestionsList;
+        public SurveyQuestion GetQuestion(int index) => surveyQuestionsList[index];
+    }
+}
---
+++ NUlableIntroduction/SurveyResponse.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NUlableIntroduction
+{
+    public class SurveyResponse
+    {
+        public int Id { get; }
+        public SurveyResponse(int id) => Id = id;
+
+        public static readonly Random randomGenerator = new Random();
+
+        public static SurveyResponse GetRandomId() =>
+            new SurveyResponse(randomGenerator.Next());
+
+        private Dictionary<int, string>? surveyResponses;
+        public bool AnsweredSurvey => surveyResponses != null;
+        public string Answer(int index) => surveyResponses?.GetValueOrDefault(index) ?? "No answer";
+
+        public bool AnswerSurvey(IEnumerable<SurveyQuestion> questions)
+        {
+            if (ConsentToSurvey())
+            {
+                surveyResponses = new Dictionary<int, string>();
+                int index = 0;
+                foreach (var question in questions)
+                {
+                    var answer = GenerateAnswer(question);
+                    if (answer != null)
+                    {
+                        surveyResponses.Add(index, answer);
+                    }
+                    index++;
+                }
+            }
+            return surveyResponses != null;
+        }
+
+        private bool ConsentToSurvey() => randomGenerator.Next(0, 2) == 1;
+
+        private string? GenerateAnswer(SurveyQuestion question)
+        {
+            switch (question.QuestionType)
+            {
+                case QuestionType.YesNo:
+                    int n = randomGenerator.Next(-1, 2);
+                    return (n == -1) ? default : (n == 0) ? "No" : "Yes";
+                case QuestionType.Number:
+                    n = randomGenerator.Next(-30, 101);
+                    return (n < 0) ? default : n.ToString();
+                case QuestionType.Text:
+
+
+                default:
+                    switch (randomGenerator.Next(0, 5))
+                    {
+                        case 0:
+                            return default;
+                        case 1:
+                            return "Red";
+                        case 2:
+                            return "Green";
+                        case 3:
+                            return "Blue";
+                        case 4:
+                            return "Blue";
+                    }
+                   return "Red. No, Green. Wait.. Blue... AAARGGGGGHHH!";
+            }
+
+        }
+    }
+}
